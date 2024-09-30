bdfeb26b57236b7d00d391f40b5f9116e24c70bd
Sakhanskyi Oleksandr <L20220126@SSPTAJI.CZ>
2024-09-30 13:04:18 +0200

    up

--- app.js
+++ app.js
@@ -13,3 +13,46 @@ if ('serviceWorker' in navigator) {
 function sayHello() {
     document.getElementById('hello').innerText = 'Hello world';
 }
+
+    
+    function calculate() {
+        
+        const number1 = parseFloat(document.getElementById('number1').value);
+        const operation = document.getElementById('operation').value;
+        const number2 = parseFloat(document.getElementById('number2').value);
+        const resultDisplay = document.getElementById('result');
+        let result;
+
+       
+        if (isNaN(number1) || isNaN(number2)) {
+            resultDisplay.textContent = "Zadej obě čísla!";
+            return;
+        }
+
+        
+        switch (operation) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                
+                if (number2 === 0) {
+                    resultDisplay.textContent = "Nelze dělit nulou!";
+                    return;
+                }
+                result = number1 / number2;
+                break;
+            default:
+                resultDisplay.textContent = "Neznámá operace!";
+                return;
+        }
+
+        
+        resultDisplay.textContent = "Výsledek: " + result;
+    }
